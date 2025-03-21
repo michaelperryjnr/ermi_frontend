dfddf619e540f6371e21383e59032f641bbef13c
michaelperryjnr <michaelperryt97@gmail.com>
2025-03-21 09:43:39 +0000

    Added video placeholder

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,11 +1,13 @@
 import logo from "./logo.png";
 import landingHero from "./landing-hero.jpg";
 import headPastor from "./pastor.jpg";
+import videoPlaceholder from "./videoPlaceholder.png";
 
 const Images = {
   logo,
   landingHero,
   headPastor,
+  videoPlaceholder,
 };
 
 export default Images;
